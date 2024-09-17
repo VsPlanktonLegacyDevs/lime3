5bed30296b139a2c28633abbd66dd33715d62000
Lily <egzozu.be.bas@gmail.com>
2024-09-17 19:42:56 +0300

    Update GameActivity.java

--- templates/android/template/app/src/main/java/org/haxe/lime/GameActivity.java
+++ templates/android/template/app/src/main/java/org/haxe/lime/GameActivity.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.MimeTypeMap;
 import org.haxe.extension.Extension;
 import org.libsdl.app.SDLActivity;
@@ -106,6 +107,22 @@ public class GameActivity extends SDLActivity {
 
 
 	protected void onCreate (Bundle state) {
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+
+			getWindow ().addFlags (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+			getWindow ().getAttributes ().layoutInDisplayCutoutMode =
+				WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
+
+		} else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+
+			getWindow ().addFlags (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+			getWindow ().getAttributes ().layoutInDisplayCutoutMode =
+				WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+
+		}
 
 		super.onCreate (state);
 
